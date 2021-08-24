6c3b3f41cc9cbe7a6e114b1827cc8d80233f5c80
shenwl <shenweilun96@gmail.com>
2021-08-24 22:42:01 +0800

    3sum: 外层循环+内层双指针

--- leetcode/15.3sum.js
+++ leetcode/15.3sum.js
@@ -5,43 +5,88 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 回溯解法，leetcode超时
+// function equal(arr1, arr2) {
+//   if (!Array.isArray(arr1) || !Array.isArray(arr2)) return false;
+//   if (arr1.length !== arr2.length) return;
+//   const copy1 = arr1.sort();
+//   const copy2 = arr2.sort();
+//   for (let i = 0; i < copy1.length; i++) {
+//     if (copy1[i] !== copy2[i]) return false;
+//   }
+
+//   return true;
+// }
+
+// function sum(...args) {
+//   return args.reduce((ans, cur) => {
+//     return ans + cur;
+//   }, 0)
+// }
+// var threeSum = function (nums) {
+//   if (nums.length <= 2) return [];
+
+//   const ans = [];
+
+//   const trace = (indices, end) => {
+//     if (end >= nums.length - (3 - indices.length)) return;
+//     if (indices.length === 3) {
+//       const list = indices.map(i => nums[i]);
+//       if (sum(...list) === 0 && !ans.some(arr => equal(arr, list))) {
+//         ans.push(list);
+//       }
+//       return;
+//     }
+
+//     for (let i = end + 1; i < nums.length; i++) {
+//       trace([...indices, i], i)
+//     }
+//   }
+
+//   for (let i = 0; i < nums.length; i++) {
+//     trace([i], i);
+//   }
+
+//   return ans;
+// };
 var threeSum = function (nums) {
-  if (nums.length < 3) return [];
+  if (nums.length < 2) return [];
+
+  const sortedNums = [...nums].sort();
 
   const ans = [];
+  for (let i = 0; i < sortedNums.length - 2; i++) {
+    const target = sortedNums[i];
+    if (target > 0) break;
+    if (i > 0 && target === sortedNums[i - 1]) continue;
 
-  const trace = (indices, end) => {
-    if (end >= nums.length) return;
-    if (indices.length === 3) {
-      const list = indices.map(i => nums[i]);
-      if (sum(...list) === 0 && !ans.some(arr => equal(arr, list))) {
-        ans.push(list);
+    let left = i + 1;
+    let right = sortedNums.length - 1;
+
+    // 找到left + right + target === 0;
+    while (left < right) {
+      const sum = sortedNums[left] + sortedNums[right] + target;
+      if (sum > 0) {
+        right--;
+
+        while (sortedNums[right - 1] === sortedNums[right]) right--;
+      } else if (sum < 0) {
+        left++;
+
+        while (sortedNums[left + 1] === sortedNums[left]) left++;
+      } else {
+        ans.push([target, sortedNums[left], sortedNums[right]]);
+        left++;
+        right--;
+
+        while (sortedNums[left + 1] === sortedNums[left]) left++;
+        while (sortedNums[right - 1] === sortedNums[right]) right--;
       }
-      return;
     }
-    for (let i = end + 1; i < nums.length; i++) {
-      trace([...indices, i], i)
-    }
-  }
 
-  for (let i = 0; i < nums.length - 3; i++) {
-    trace([i], i)
   }
-
   return ans;
 };
 
-function equal(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return false;
-  if (arr1.length !== arr2.length) return;
-
-  return JSON.stringify([...arr1].sort()) === JSON.stringify([...arr2].sort());
-}
-
-function sum(...args) {
-  return args.reduce((ans, cur) => {
-    return ans + cur;
-  }, 0)
-}
-
 console.log(threeSum([-1, 0, 1, 2, -1, -4])) // [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0, 0, 0])) // [[0,0,0]]
