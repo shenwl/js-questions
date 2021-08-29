68a294b85be76000f3d19ef9af59a311ce0b8060
shenwl <shenweilun96@gmail.com>
2021-08-29 21:33:29 +0800

    二分搜索

---
+++ leetcode/704.binarySearch.js
@@ -0,0 +1,19 @@
+var binarySearch = function (nums, target) {
+  let l = 0, r = nums.length - 1;
+
+  while (r > l) {
+    let mid = Math.floor((l + r) / 2);
+    console.log(l, mid, r)
+
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      l = mid;
+    } else {
+      r = mid;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9))
